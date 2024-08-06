e26c60b1d28b4a6b4e1d8343ca335f8a47f5913e
Jeffrey Faber <c1a9r8d5.com>
2024-08-05 20:51:02 -0400

    s

--- heatmap.js
+++ heatmap.js
@@ -19,8 +19,8 @@ const BODY_REGIONS = {
         rightUpperArm: { x: 135, y: 115, width: 30, height: 40 },
         leftElbow: { x: 30, y: 145, width: 35, height: 20 },  // Moved slightly outward
         rightElbow: { x: 140, y: 145, width: 35, height: 20 },  // Moved slightly outward
-        leftForearm: { x: 20, y: 170, width: 40, height: 40 },  // Moved further outward
-        rightForearm: { x: 150, y: 170, width: 40, height: 40 },  // Moved further outward
+        leftForearm: { x: 20, y: 165, width: 40, height: 40 },  // Moved further outward
+        rightForearm: { x: 150, y: 165, width: 40, height: 40 },  // Moved further outward
         leftWrist: { x: 20, y: 255, width: 40, height: 15 },  // Moved further outward
         rightWrist: { x: 150, y: 255, width: 40, height: 15 },  // Moved further outward
         leftHand: { x: 15, y: 270, width: 45, height: 25 },  // Moved further outward
@@ -149,8 +149,8 @@ function fetchDataFromAPI() {
             abdomen: 0.1,
             leftUpperArm: 0.1,
             rightUpperArm: 0.1,
-            leftElbow: 0.1,
-            rightElbow: 0.1,
+            leftElbow: 0.3,
+            rightElbow: 0.3,
             leftForearm: 0.9,
             rightForearm: 0.9,
             leftWrist: 0.1,
