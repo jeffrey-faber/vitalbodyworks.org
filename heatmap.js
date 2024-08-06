5a72f84fc32aec5ee28ce36f66db45487618e824
Jeffrey Faber <c1a9r8d5.com>
2024-08-05 21:27:01 -0400

    s

--- heatmap.js
+++ heatmap.js
@@ -29,9 +29,9 @@ const BODY_REGIONS = {
         pelvis: { x: 81, y: 195, width: 40, height: 30 },
         thighs: { x: 60, y: 225, width: 80, height: 60 },
         knees: { x: 60, y: 285, width: 80, height: 25 },
-        shins: { x: 60, y: 310, width: 80, height: 30 },
-        ankle: { x: 60, y: 340, width: 80, height:25 },
-        feet: { x: 60, y: 385, width: 80, height: 35 }
+        shins: { x: 60, y: 310, width: 80, height: 40 },
+        ankle: { x: 60, y: 350, width: 80, height:25 },
+        feet: { x: 60, y: 395, width: 80, height: 35 }
     }    ,    
     sideRegions: {
         head: { x: 10, y: 0, width: 55, height: 75 },
